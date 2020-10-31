c8661115e3e5d574c36caaca5a40b98e5aa27e1f
annemoraes <ilarymoraes@hotmail.com>
2020-10-31 01:31:31 -0300

    adição de repositorios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import "./styles.css";
+import api from './services/api';
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'aktie',
+      url: 'https://github.com/annemoraes/aktie',
+      techs: ['Node.js', 'ReactJs']
+    });
+    setRepositories([ ...repositories, response.data ]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +26,15 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repository => (
+          <li key={repository.id}>
+          {repository.title}
 
           <button onClick={() => handleRemoveRepository(1)}>
             Remover
           </button>
-        </li>
+        </li>  
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
